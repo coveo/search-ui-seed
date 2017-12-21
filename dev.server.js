8616b55521ed8a02110a601aae2f108789a72bba
Eugene Komisarenko <ekomisarenko@gmail.com>
2017-12-21 09:21:16 -0500

    Fixed breaking change, integrated with Webpack 2
    Fixed breaking change introduced by the upgrade to Webpack 2 in the following changeset https://github.com/coveo/search-ui-seed/commit/b475908b8bf9d165977638a72a97dc332452d1b3#diff-598ab6b4b8c894aaaa88bc22bd04aab9.

--- dev.server.js
+++ dev.server.js
@@ -8,7 +8,7 @@ const argv = require('yargs').argv
 const port = argv.port || 8080;
 
 let webpackConfig = require('./webpack.config.js');
-webpackConfig.entry.unshift(`webpack-dev-server/client?http://localhost:${port}/`);
+webpackConfig.entry['webpack_hm_client'] = `webpack-dev-server/client?http://localhost:${port}/`;
 const compiler = webpack(webpackConfig);
 
 var server = new WebpackDevServer(compiler, {
